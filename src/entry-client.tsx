1c294076bafd891b48050c07dd357a7b8e3174e9
jackeraf <jarekimar@yahoo.es>
2026-02-05 22:08:50 +0100

    spa workflow deploy

--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { hydrateRoot, createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import ErrorBoundary from "./components/ErrorBoundary";
 import Routes from "./routes/Routes";
@@ -13,9 +13,9 @@ const app = (
   <StrictMode>
     <ErrorBoundary>
       <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
+        <HashRouter basename="/">
           <Routes />
-        </BrowserRouter>
+        </HashRouter>
       </QueryClientProvider>
     </ErrorBoundary>
   </StrictMode>
